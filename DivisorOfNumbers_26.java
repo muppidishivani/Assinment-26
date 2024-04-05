49978ce696f652a512a651c6181bfab8a9483c0c
muppidishivani <133558846+muppidishivani@users.noreply.github.com>
2024-04-05 07:48:13 +0530

    Add files via upload

---
+++ DivisorOfNumbers_26.java
@@ -0,0 +1,23 @@
+package com.web.java;
+
+import java.util.Scanner;
+
+public class DivisorOfNumbers_26 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int number=sc.nextInt();
+		for(int i=1;i<=number;i++)
+		{
+			if(number%i==0)
+			{
+				System.out.println("Divisors of a given number="+i);
+			}
+		}
+		sc.close();
+
+	}
+
+
+}
